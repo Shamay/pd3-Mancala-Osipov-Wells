7cbca50937d2147829ed7afb1734291d1ae30b19
Julie Nix <julienix@Malcolms-Mac-mini.local>
2014-06-15 20:33:26 -0400

    Initial AI created, compiles but not tested

---
+++ Opponent.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Opponent extends Player{
+    public Opponent(boolean side){
+	score = 0;
+	this.side = side;
+    }
+    public void takeTurn(){
+	
+}
